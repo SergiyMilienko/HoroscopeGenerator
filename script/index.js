cd93dd87b12f4031859d44985de70ea3a6d33e7b
SergiyMilienko <sergiymilienko@gmail.com>
2023-08-05 10:48:47 +0300

    added randomness

--- script/index.js
+++ script/index.js
@@ -2,9 +2,26 @@
 const generateHoroscopeBtn = document.getElementById("generateHoroscope");
 const horoscopeOutput = document.getElementById("horoscopeOutput");
 
+const greetings = [
+    "Hello!", "Hi there!", "Greetings!", "Hey!", "Welcome!"
+];
+
+const subjects = [
+    "You", "Your friend", "Everyone", "Someone special", "A stranger"
+];
+
+const actions = [
+    "will have a great day.", "will receive exciting news.", "should take a break and relax.",
+    "will meet someone new.", "will achieve your goals.", "will find luck in unexpected places."
+];
+
 // Function to generate the horoscope (replace this with your logic)
 function generateHoroscope() {
-    const horoscope = "Today's horoscope: You will have a great day!";
+    const randomGreeting = greetings[Math.floor(Math.random() * greetings.length)];
+    const randomSubject = subjects[Math.floor(Math.random() * subjects.length)];
+    const randomAction = actions[Math.floor(Math.random() * actions.length)];
+
+    const horoscope = `${randomGreeting} ${randomSubject} ${randomAction}`;
     return horoscope;
 }
 
